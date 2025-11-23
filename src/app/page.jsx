f7fc1aae1666c8199495d70509ee107b5b3b87e1
Justin McCrea <mammerjam303@gmail.com>
2025-11-23 08:56:37 -0700

    Create page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Rangi’s Heartbeat: Vercel Starter</h1>
+      <p>Your Next.js app is deployed successfully.</p>
+    </main>
+  );
+}
